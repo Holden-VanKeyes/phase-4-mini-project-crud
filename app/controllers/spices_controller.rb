d9ddf42ad6e1aa93c3a8fa7cd81a71017cbaf3af
Abram Shook <ajshook77@gmail.com>
2022-06-30 10:53:07 -0500

    commit tree 38545c3915ab61836399f9f03a5af4733af55211

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,40 @@
+class SpicesController < ApplicationController
+
+    def index
+        
+    spice = Spice.all
+    render json: spice
+      
+    end
+
+    # def show
+        
+    #     spice = Spice.all
+    #     render json: spice
+          
+    # end
+
+   
+
+    def destroy
+     spice = Spice.find(params[:id])
+     spice.destroy
+    end
+
+    def create
+     spice = Spice.create(spice_params)
+    render json: spice, status: :created
+    end
+
+    def update
+     spice = Spice.find(params[:id])
+     spice.update(spice_params)
+     render json: spice
+    end
+
+    private
+
+    def spice_params
+     params.permit(:title, :image, :description, :notes, :rating)
+    end
+end
